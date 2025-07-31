e84122ec7118756d4a56d6281a87d174b06d6fa1
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-07-31 13:59:43 +0530

    added: animations in appit along with full hero banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { HeroBanner } from "@/components/pages/home/sections/hero-banner"
 import { HeroSection } from "@/components/pages/home/sections/hero-section"
 
 export default function HomePage() {
-  return <HeroSection />
+  return (
+    <>
+      <HeroSection />
+      <HeroBanner />
+    </>
+  )
 }
--- components/pages/home/sections/hero-section/index.tsx
+++ components/pages/home/sections/hero-section/index.tsx
@@ -2,23 +2,67 @@ import { Container } from "@/components/global/container"
 import { AvatarCircle } from "./avatar-circle"
 import { WaitListButton } from "@/components/buttons/wait-list-button"
 import { SocialPlatforms } from "@/components/pages/home/sections/hero-section/social-platforms"
-import { HeroBanner } from "@/components/pages/home/sections/hero-banner"
+import { TextEffect } from "@/components/motion/text-effect"
+import { AnimatedGroup } from "@/components/motion/animated-group"
+import { transitionVariants } from "@/constants/transition-variants"
 
 export const HeroSection = () => {
   return (
-    <Container className="my-20 flex items-center justify-center flex-col gap-3">
+    <Container className="mt-20 flex items-center justify-center flex-col gap-3">
       <AvatarCircle />
       <div className="gap-3 flex items-center flex-col">
-        <h1 className="text-center text-6xl font-extrabold leading-tight">
-          Connect, Share, and Grow <br /> with Appit Social
-        </h1>
-        <p className="text-center text-xl text-muted-foreground">
-          Join Appit to build authentic connections and share
-          <br /> your passions effortlessly.
-        </p>
-        <WaitListButton className="mt-5" />
-        <SocialPlatforms />
-        <HeroBanner />
+        <TextEffect
+          as="h1"
+          preset="fade-in-blur"
+          speedSegment={0.7}
+          className="text-center text-6xl font-extrabold leading-tight mx-auto max-w-[50rem]"
+        >
+          Connect, Share, and Grow with Appit Social
+        </TextEffect>
+        <TextEffect
+          as="p"
+          per="line"
+          delay={0.5}
+          preset="fade-in-blur"
+          speedSegment={0.7}
+          className="text-center text-xl text-muted-foreground mx-auto max-w-lg"
+        >
+          Join Appit to build authentic connections and share your passions
+          effortlessly.
+        </TextEffect>
+
+        <AnimatedGroup
+          as="div"
+          variants={{
+            container: {
+              visible: {
+                transition: {
+                  staggerChildren: 0.05,
+                  delayChildren: 0.4,
+                },
+              },
+            },
+            ...transitionVariants,
+          }}
+        >
+          <WaitListButton className="mt-5" />
+        </AnimatedGroup>
+        <AnimatedGroup
+          as="div"
+          variants={{
+            container: {
+              visible: {
+                transition: {
+                  staggerChildren: 0.05,
+                  delayChildren: 0.4,
+                },
+              },
+            },
+            ...transitionVariants,
+          }}
+        >
+          <SocialPlatforms />
+        </AnimatedGroup>
       </div>
     </Container>
   )

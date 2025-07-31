7897b2277815b81bb25f407a5521b13ab1c378c6
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-07-31 19:01:58 +0530

    added: social experience section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { HeroBanner } from "@/components/pages/home/sections/hero-banner"
 import { HeroSection } from "@/components/pages/home/sections/hero-section"
+import { SocialExperience } from "@/components/pages/home/sections/hero-section/social-experience"
 import { TrustedCompanies } from "@/components/pages/home/sections/hero-section/trusted-companies"
 
 export default function HomePage() {
@@ -8,6 +9,7 @@ export default function HomePage() {
       <HeroSection />
       <HeroBanner />
       <TrustedCompanies />
+      <SocialExperience />
     </>
   )
 }
---
+++ components/pages/home/sections/hero-section/social-experience.tsx
@@ -0,0 +1,95 @@
+"use client"
+
+import { Container } from "@/components/global/container"
+import { AnimatedNumber } from "@/components/motion-primitives/animated-number"
+import Image from "next/image"
+import { useEffect, useState } from "react"
+
+export const SocialExperience = () => {
+  const [value, setValue] = useState({
+    firstValue: 450,
+    secondValue: 150,
+    thirdValue: 0,
+  })
+
+  useEffect(() => {
+    setValue({
+      firstValue: 500,
+      secondValue: 200,
+      thirdValue: 50,
+    })
+  }, [])
+
+  return (
+    <Container className="mt-56 flex items-center justify-center flex-col gap-8 mx-auto max-w-6xl">
+      <div className="flex items-center justify-center size-24 rounded-full bg-gradient-to-b from-zinc-800 to-black">
+        <Image
+          src="/app-logo.svg"
+          alt="app-logo"
+          className="size-14"
+          width={1000}
+          height={1000}
+          sizes="100vw"
+        />
+      </div>
+
+      <p className="text-3xl md:text-5xl text-center font-bold ">
+        Empowering authentic connections for a vibrant social experience.
+      </p>
+
+      <div className="grid grid-cols-1 min-[818px]:grid-cols-3 gap-3 mt-20">
+        <div className="flex items-center justify-center flex-col gap-4 border-b min-[818px]:border-r p-4">
+          <span className="text-5xl font-bold">
+            <AnimatedNumber
+              springOptions={{
+                bounce: 0,
+                duration: 2000,
+              }}
+              value={value.firstValue}
+            />
+            k
+          </span>
+          <p className="text-muted-foreground text-lg text-center">
+            Shared moments and stories{" "}
+            <br className="min-[818px]:hidden inline-block" /> every month
+          </p>
+        </div>
+
+        <div className="flex items-center justify-center flex-col gap-4 border-b min-[818px]:border-r p-4">
+          <span className="text-5xl font-bold">
+            <AnimatedNumber
+              springOptions={{
+                bounce: 0,
+                duration: 2000,
+              }}
+              value={value.secondValue}
+            />
+            k
+          </span>
+          <p className="text-muted-foreground text-lg text-center">
+            Communities thriving across
+            <br className="min-[818px]:hidden inline-block" /> diverse interests
+          </p>
+        </div>
+
+        <div className="flex items-center justify-center flex-col gap-4 p-4">
+          <span className="text-5xl font-bold">
+            <AnimatedNumber
+              springOptions={{
+                bounce: 0,
+                duration: 2000,
+              }}
+              value={value.thirdValue}
+            />
+            k
+          </span>
+          <p className="text-muted-foreground text-lg text-center">
+            Countries where Appit is{" "}
+            <br className="min-[818px]:hidden inline-block" />
+            growing strong
+          </p>
+        </div>
+      </div>
+    </Container>
+  )
+}
